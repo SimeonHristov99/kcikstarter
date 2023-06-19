31fbfbf5cc3e86b331b4ece10fe1b85cd12f5e40
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-06-19 19:35:30 +0300

    Load data.

--- 01_Scripts/controller.py
+++ 01_Scripts/controller.py
@@ -4,23 +4,21 @@ and the file structure."""
 import os
 import warnings
 
-FULL_PROJECT_PATH = os.getcwd()
+FULL_PROJECT_PATH = r"/home/work/Dev/kickstarter"
 
 
 class Controller:
-    """Defines the logic of the `Controller` class
-    """
+    """Defines the logic of the `Controller` class"""
 
     def __init__(self, iteration: str) -> None:
         self.iteration = iteration
-        self.path_data_original = fr'{FULL_PROJECT_PATH}/00_Data/Original Data/'
-        self.path_data_prepared = fr'{FULL_PROJECT_PATH}/00_Data/Prepared Data/'
-        self.path_scripts = fr'{FULL_PROJECT_PATH}/01_Scripts/'
-        self.path_outputs = fr'{FULL_PROJECT_PATH}/02_Outputs/'
+        self.path_data_original = rf"{FULL_PROJECT_PATH}/00_Data/Original Data/"
+        self.path_data_prepared = rf"{FULL_PROJECT_PATH}/00_Data/Prepared Data/"
+        self.path_scripts = rf"{FULL_PROJECT_PATH}/01_Scripts/"
+        self.path_outputs = rf"{FULL_PROJECT_PATH}/02_Outputs/"
 
     def create_folder_structure(self) -> None:
-        """Creates the file structure used in the project
-        """
+        """Creates the file structure used in the project"""
         os.makedirs(self.path_data_original, exist_ok=True)
         os.makedirs(self.path_data_prepared, exist_ok=True)
         os.makedirs(self.path_scripts, exist_ok=True)
@@ -33,8 +31,7 @@ class Controller:
             str: Path to the directory holding the original data.
         """
         if not os.path.isdir(self.path_data_original):
-            warnings.warn(
-                f'Directory {self.path_data_original} may not exist.')
+            warnings.warn(f"Directory {self.path_data_original} may not exist.")
         return self.path_data_original
 
     def get_path_data_prepared(self) -> str:
@@ -44,8 +41,7 @@ class Controller:
             str: Path to the directory holding the original data.
         """
         if not os.path.isdir(self.path_data_prepared):
-            warnings.warn(
-                f'Directory {self.path_data_prepared} may not exist.')
+            warnings.warn(f"Directory {self.path_data_prepared} may not exist.")
         return self.path_data_prepared
 
     def get_path_iteration(self, iteration=None) -> str:
@@ -61,11 +57,11 @@ class Controller:
         if iteration is None:
             iteration = self.iteration
 
-        path = fr'{self.path_outputs}{iteration}'
+        path = rf"{self.path_outputs}{iteration}"
         os.makedirs(path, exist_ok=True)
         return path
 
 
-if __name__ == '__main__':
-    c = Controller('i01')
+if __name__ == "__main__":
+    c = Controller("i01")
     c.create_folder_structure()
